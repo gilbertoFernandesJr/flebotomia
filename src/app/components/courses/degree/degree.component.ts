b528991dd0b5030361111a4ca1e78532469bc40b
Fernandes Júnior <fernaandes.jr@gmail.com>
2023-07-20 18:47:44 -0300

    feat: error handling in chec code degree

--- src/app/components/courses/degree/degree.component.ts
+++ src/app/components/courses/degree/degree.component.ts
@@ -3,6 +3,7 @@ import { Degree } from './../../../models/degree';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute  } from '@angular/router';
 import { DegreeService } from 'src/app/services/degree.service';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-degree',
@@ -21,6 +22,13 @@ export class DegreeComponent implements OnInit {
     }
   }
 
+  error: any = {
+    message: ''
+  };
+
+  codeFormControl = new FormControl('', [Validators.minLength(60)]);
+  codeInvalid: boolean = false;
+
   constructor(
     private service: DegreeService,
     private route: ActivatedRoute
@@ -28,22 +36,34 @@ export class DegreeComponent implements OnInit {
 
   ngOnInit(): void {
     this.getCode();
-    console.log("CHEGUEI AO DEGREE COMPONENT");
   }
 
-  findByCode(code: string) {
-    this.service.findByCode(code).subscribe(response => {
-      this.degree = response;
-    })
+  private findByCode(code: string): void {
+    this.service.findByCode(code).subscribe({
+      next: (res) => this.degree = res,
+      error: (e) => this.error = e.error
+    });
   }
 
-  private getCode() {
+  private getCode(): void {
     var code: string = "";
     this.route.params.subscribe(params => {
       code = params['code'];
     })
     console.log("código: "+code);
     this.findByCode(code);
+  }
+
+  public tryForInput(): void {
+    if (this.codeFormControl.valid) {
+      this.findByCode(this.codeFormControl.value);
+      this.codeInvalid = false;
+      this.error = {
+        message: ''
+      };
+    } else {
+      this.codeInvalid = true;
+    }
   }
 
 }
